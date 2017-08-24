aa64bc4abdf7b10d903b174fde72c7a7f80f177d
jtviegas <jtviegas.js@google.com>
2017-08-24 18:23:24 +0100

    ...refactoring

--- scripts/runLocal.sh
+++ scripts/runLocal.sh
@@ -10,6 +10,7 @@ docker rm $CONTAINER
 
 # docker run -d --name $CONTAINER --link $KAFKA_CONTAINER:$KAFKA_HOST --link $INFLUXDB_CONTAINER:$INFLUXDB_HOST --env AGENT_NAME=$AGENT -v $base_folder/tmp:/var/log/flume $IMG 
 docker run -d -h $HOST --name $CONTAINER --link $KAFKA_CONTAINER:$KAFKA_HOST \
+	--link $ZK_CONTAINER:$ZK_HOST \
 	--link $INFLUXDB_CONTAINER:$INFLUXDB_HOST --env AGENT_NAME=$AGENT --env DEBUG_PORT=$DEBUG_PORT \
 	-p $DEBUG_PORT:$DEBUG_PORT $DOCKER_HUB_IMG:$IMG_VERSION
 # docker logs -f $CONTAINER
